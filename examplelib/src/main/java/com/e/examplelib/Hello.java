9d464a64fa26741b73de53539923f5e1a85a177e
Ayush Kumar Singh <ayushkumarsingh06@gmail.com>
2020-08-09 15:25:02 +0530

    first commit

---
+++ examplelib/src/main/java/com/e/examplelib/Hello.java
@@ -0,0 +1,36 @@
+package com.e.examplelib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Hello extends AppCompatActivity {
+
+    Button btnNext;
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+
+        btnNext = findViewById(R.id.next);
+        next(btnNext, MainActivity.class);
+
+    }
+
+    void next(Button btn, final Class<? extends Activity> activity){
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(context, activity));
+            }
+        });
+    }
+}
